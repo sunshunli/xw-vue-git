7411d7b3bf77b95eff42c11e14eb9d9a74584188
Stringbao <string_bao@163.com>
2019-03-05 15:23:42 +0800

    1

--- admin/src/core/comps/tree/treeItem.vue
+++ admin/src/core/comps/tree/treeItem.vue
@@ -44,7 +44,26 @@ export default {
                 let _url  = this.asynOptions.getUrl(item);
                 //发送ajax请求, 改变loading状态
                 item.__cls = "fa-caret-load";
-                window.setTimeout(()=>{
+                // window.setTimeout(()=>{
+                //     let tmp = this.asynOptions.analysis && this.asynOptions.analysis(this.getTestData(item));
+                    
+                //     //通知root节点，有数据变化，自己本身节点不做任何改变(不能改变自身对象)
+                //     let tmpObject = {actionKey:KEYS.ACTIONKEY.UPDATECHILDREN,__tmpId:item.__tmpId,data:{}};
+                //     if(tmp && tmp instanceof Array && tmp.length != 0){
+                //         let tmpData = KEYS.INITATTRIBUTE(tmp,item,false);
+                //         tmpObject.data.children = tmpData;
+                //         tmpObject.data.hasChildren = true;
+                //         tmpObject.data.expand = true;
+                //         tmpObject.data.cls = "fa-caret-down";
+                //     }else{
+                //         tmpObject.data.children = [];
+                //         tmpObject.data.hasChildren = false;
+                //         tmpObject.data.expand = false;
+                //         tmpObject.data.cls = "fa-caret-left";
+                //     }
+                //     _eventPublisher.broadcast(this.EVENTPUBLISHKEY,tmpObject);
+                // },100)
+                this.ajax.getFetch(_url).then(d=>{
                     let tmp = this.asynOptions.analysis && this.asynOptions.analysis(this.getTestData(item));
                     
                     //通知root节点，有数据变化，自己本身节点不做任何改变(不能改变自身对象)
@@ -62,12 +81,7 @@ export default {
                         tmpObject.data.cls = "fa-caret-left";
                     }
                     _eventPublisher.broadcast(this.EVENTPUBLISHKEY,tmpObject);
-                },100)
-                // this.ajax.getFetch(_url).then(d=>{
-                //     let tmp = this.asynOptions.analysis(d);
-                //     if(tmp && tmp.length != 0){
-                //     }
-                // })
+                })
             }else{
                 console.log("展开折叠操作");
                 let cls = "";

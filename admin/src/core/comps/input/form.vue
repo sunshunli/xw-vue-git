1b0f4c6774e170abddd8d6238f04047b89d1fb18
Stringbao <string_bao@163.com>
2019-03-07 18:52:37 +0800

    1

--- admin/src/core/comps/input/form.vue
+++ admin/src/core/comps/input/form.vue
@@ -11,13 +11,31 @@
 export default {
     name:"LeForm",
     data(){
-        return {}
+        return {
+            subComps:[]
+        }
     },
     methods:{
-
+        getAllSubComponents(arr){
+            if(arr && arr instanceof Array && arr.length >=1){
+                for(let i =0;i< arr.length;i++){
+                    if(arr[i].leInputValidate){
+                        this.subComps.push(arr[i]);
+                    }
+                    
+                    let children = arr[i].$children;
+                    if(children.length > 0){
+                        this.getAllSubComponents(children);
+                    }
+                }
+            }else{
+                return;
+            }
+        }
     },
     mounted(){
         debugger
+        let comps = this.getAllSubComponents(this.$children);
     }
 }
 </script>

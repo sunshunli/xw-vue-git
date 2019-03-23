2a0f289979b21fc722cb039e182f4b621ba5bb7b
Stringbao <string_bao@163.com>
2019-03-23 16:00:22 +0800

    开发日期控件

--- admin/src/core/comps/datepicker/date.vue
+++ admin/src/core/comps/datepicker/date.vue
@@ -11,19 +11,19 @@
         <div class="picker-box">
             <div class="picker-header">
                 <button>
-                    <i class="fa fa-angle-double-left"></i>
+                    <i class="fa fa-angle-double-left" @click="prevYear"></i>
                 </button>
                 <button>
-                    <i class="fa fa-angle-left"></i>
+                    <i class="fa fa-angle-left" @click="prevMonth"></i>
                 </button>
                 <div class="hearderText">
-                    2019/03
+                    {{state.currentYear}}/{{state.currentMonth}}
                 </div>
                 <button>
-                    <i class="fa fa-angle-right"></i>
+                    <i class="fa fa-angle-right" @click="nextMonth"></i>
                 </button>
                 <button>
-                    <i class="fa fa-angle-double-right"></i>
+                    <i class="fa fa-angle-double-right" @click="nextYear"></i>
                 </button>
             </div>
             <div class="picker-body">
@@ -31,50 +31,15 @@
                     <tr>
                         <th v-for="(item,index) in Weeks" :key="index">{{item}}</th>
                     </tr>
-                    <tr>
-                        <td class="disable">30</td>
+                    <tr v-for="(item,index) in state.data" :key="index">
+                        <td :class="x.month != state.currentMonth?'disable':''" v-for="(x,idx) in item" :key="idx">{{x.day}}</td>
+                        <!-- <td class="disable">30</td>
                         <td>1</td>
                         <td class="current">2</td>
                         <td>3</td>
                         <td>4</td>
                         <td>5</td>
-                        <td>6</td>
-                    </tr>
-                    <tr>
-                        <td>7</td>
-                        <td>8</td>
-                        <td>9</td>
-                        <td>10</td>
-                        <td>11</td>
-                        <td>12</td>
-                        <td>13</td>
-                    </tr>
-                    <tr>
-                        <td>14</td>
-                        <td>15</td>
-                        <td>16</td>
-                        <td>17</td>
-                        <td>18</td>
-                        <td>19</td>
-                        <td>20</td>
-                    </tr>
-                    <tr>
-                        <td>21</td>
-                        <td>22</td>
-                        <td>23</td>
-                        <td>24</td>
-                        <td>25</td>
-                        <td>26</td>
-                        <td>27</td>
-                    </tr>
-                    <tr>
-                        <td>28</td>
-                        <td>29</td>
-                        <td>30</td>
-                        <td>31</td>
-                        <td class="disable">1</td>
-                        <td class="disable">2</td>
-                        <td class="disable">3</td>
+                        <td>6</td> -->
                     </tr>
                 </table>
             </div>
@@ -85,14 +50,36 @@
 <script>
 
 const _tool = {
+    init(year,month){
+
+    },
+    /**
+     * @description 根据年月获取当前月有多少天
+     * @param year:年份
+     * @param month:月份
+     * @returns 天数:int
+     */
     getDays(year,month){
         let date = new Date(year,month,0);
         return date.getDate();
     },
+    /**
+     * @description 根据年月获取当前月的第一天是星期几
+     * @param year:年份
+     * @param month:月份
+     * @returns 第一天是周几:int
+     */
     getWeek(year,month){
-        let data = new Date(year,month,"01");
+        let str = year + "," + month + "," + "01";
+        let data = new Date(str);
         return data.getDay();
     },
+    /**
+     * @description 根据当前月份的总天数
+     * @param year:年份
+     * @param month:月份
+     * @returns days:int
+     */
     getCurrentDays(){
         let date = new Date();
         let year = date.getFullYear();
@@ -100,23 +87,108 @@ const _tool = {
         return _tool.getDays(year,month);
     },
     /**
+     * @description 根据月份获取上个月的年月日数据
+     * @param year:年份
+     * @param month:月份
+     * @returns json:{year:0,month:0,days:0}
+     */
+    getPrevMonthDays(year,month){
+        let res = {year:0,month:0,days:0};
+        if(month == 1){
+            res.year = year - 1;
+            res.month = 12;
+            res.days = this.getDays(res.year,res.month);
+        }else{
+            res.year = year;
+            res.month = month - 1;
+            res.days = this.getDays(res.year,res.month);
+        }
+        return res;
+    },
+    /**
+     * @description 根据月份获取下个月的年月日数据
+     * @param year:年份
+     * @param month:月份
+     * @returns json:{year:0,month:0,days:0}
+     */
+    getNextMonthDays(year,month){
+        let res = {year:0,month:0,days:0};
+        if(month == 12){
+            res.year = year + 1;
+            res.month = 1;
+            res.days = this.getDays(res.year,res.month);
+        }else{
+            res.year = year;
+            res.month = month + 1;
+            res.days = this.getDays(res.year,res.month);
+        }
+        return res;
+    },
+    /**
      * @description 组装日历面板上面的 5*7 = 35天所有数据源
      * @param year:年份
      * @param month:月份
      * @returns Array[]
      */
-    getFullData(){
-        
+    getFullData(year,month){
+        let res = [];
+        //获取当前月第一天是周几
+        let week = this.getWeek(year,month);
+        //获取当前月一起多少天
+        let currentDays = this.getDays(year,month);
+        //上个月的数据条数
+        let prevDaylen = week - 1;
+        //获取上一个月有多少天
+        let prevMonthDays = this.getPrevMonthDays(year,month);
+        let allData = [];
+        //push上个月填充的数据
+        for(let i = prevDaylen;i>0;i--){
+            allData.push({year:prevMonthDays.year,month:prevMonthDays.month,day:prevMonthDays.days - i + 1});
+        }
+        //push当前月填充的数据
+        for(let i=1;i<=currentDays;i++){
+            allData.push({year:year,month:month,day:i});
+        }
+        //push下个月剩余的数据
+        let nextDaysLen = 42 - (prevDaylen + currentDays);
+        let nextMonthDays = this.getNextMonthDays(year,month);
+        for(let i = 1;i<=nextDaysLen;i++){
+            allData.push({year:nextMonthDays.year,month:nextMonthDays.month,day:i});
+        }
+        return allData;
+    },
+    groupArray(data){
+        let res = [];
+        for(let i=0;i<6;i++){
+            let tmp = [];
+            for(var k=0;k<7;k++){
+                tmp.push(data[i*7 + k]);
+            }
+            res.push(tmp);
+        }
+        return res;
     }
 }
 
 import Week from "./config.js";
 
+const currentYear = new Date().getFullYear();
+const currentMonth = new Date().getMonth() + 1;
+
+/**
+ * @description 日期格式  6*7  
+ */
+
 export default {
     name:"LeDatePicker",
     data(){
         return {
-
+            state:{
+                currentYear:currentYear,
+                currentMonth:currentMonth,
+                data:[]
+            },
+            currentDay:0
         }
     },
     computed:{
@@ -125,10 +197,62 @@ export default {
         }
     },
     methods:{
-
+        prevYear(){
+            let year = this.state.currentYear - 1;
+            let month = this.state.currentMonth;
+            let data = _tool.getFullData(year,month);
+            this.state = {
+                currentYear:year,
+                currentMonth:month,
+                data:_tool.groupArray(data)
+            }
+        },
+        prevMonth(){
+            let year = this.state.currentYear;
+            let month = this.state.currentMonth;
+            if(month == 1){
+                month = 12;
+                year = year - 1;
+            }else{
+                month = month - 1;
+            }
+            let data = _tool.getFullData(year,month);
+            this.state = {
+                currentYear:year,
+                currentMonth:month,
+                data:_tool.groupArray(data)
+            }
+        },
+        nextMonth(){
+            let year = this.state.currentYear;
+            let month = this.state.currentMonth;
+            if(month == 12){
+                month = 1;
+                year = year + 1;
+            }else{
+                month = month + 1;
+            }
+            let data = _tool.getFullData(year,month);
+            this.state = {
+                currentYear:year,
+                currentMonth:month,
+                data:_tool.groupArray(data)
+            }
+        },
+        nextYear(){
+            let year = this.state.currentYear + 1;
+            let month = this.state.currentMonth;
+            let data = _tool.getFullData(year,month);
+            this.state = {
+                currentYear:year,
+                currentMonth:month,
+                data:_tool.groupArray(data)
+            }
+        }
     },
     mounted(){
-        let days = _tool.getCurrentDays();
+        let days = _tool.getFullData(this.state.currentYear,this.state.currentMonth);
+        this.state.data = _tool.groupArray(days);
     }
 }
 </script>

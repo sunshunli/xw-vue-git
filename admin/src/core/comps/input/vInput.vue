e7fa4a009285b4bbcb27b671a29e6dff0e1e248a
Stringbao <string_bao@163.com>
2019-05-16 16:39:57 +0800

    1

--- admin/src/core/comps/input/vInput.vue
+++ admin/src/core/comps/input/vInput.vue
@@ -49,15 +49,18 @@
 </script>
 
 <style scoped>
-    .formStyle .form-item{
-        float: left;
-        width: 100%;
-        line-height: normal;
-        margin-bottom: 24px;
-        position: relative;
-    }
+    
 
-    .formStyle .form-item .form-item-label{
+
+    
+
+
+
+
+
+
+
+    .form-item .form-item-label{
         width: 18%;
         margin-right: 10px;
         text-align: right;
@@ -69,40 +72,34 @@
         padding: 0;
         box-sizing: border-box;
     }
-
-    .formStyle .medium .form-item .form-item-label{
+    .medium .form-item .form-item-label{
         height: 40px;
         line-height: 40px;
         font-size: 14px;
     }
-
-    .formStyle .small .form-item .form-item-label{
+    .small .form-item .form-item-label{
         height: 34px;
         line-height: 34px;
         font-size: 14px;
     }
-
-    .formStyle .mini .form-item .form-item-label{
+    .mini .form-item .form-item-label{
         height: 28px;
         line-height: 28px;
         font-size: 12px;
     }
-
-    .formStyle .form-item .form-item-div{
+    .form-item .form-item-div{
         display: inline-block;
         line-height: normal;
         width: 35%;
         vertical-align: top;
     }
-
-    .formStyle .requireed::before{
+    .requireed::before{
         content: "*";
         color: #f56c6c;
         font-size: 12px;
         margin-right: 2px;
     }
-    
-    .formStyle .form-item .form-item-input{
+    .form-item .form-item-input{
         width: 88%;
         height: 40px;
         font-size: 14px;
@@ -113,110 +110,22 @@
         padding: 0 8% 0 4%;
         color: #606266;
     }
-
-    .formStyle .form-item .form-item-input:focus{
+    .form-item .form-item-input:focus{
         border: 1px solid #409eff;
     }
-
-    .formStyle .medium .form-item .form-item-input{
+    .medium .form-item .form-item-input{
         height: 38px;
         line-height: 38px;
         font-size: 14px;
     }
-
-    .formStyle .small .form-item .form-item-input{
+    .small .form-item .form-item-input{
         height: 34px;
         line-height: 34px;
         font-size: 14px;
     }
-
-    .formStyle .mini .form-item .form-item-input{
+    .mini .form-item .form-item-input{
         height: 28px;
         line-height: 28px;
         font-size: 12px;
-    }
-
-    .formStyle .form-item .promptMsg{
-        font-size: 12px;
-        color: #f56c6c;
-        line-height: 20px;
-        text-align: left;
-        position: absolute;
-    }
-
-    .formStyle .medium .fa-check-circle-o .form-item-input{
-        border: 1px solid #67c23a;
-    }
-
-    .formStyle .small .fa-check-circle-o .form-item-input{
-        border: 1px solid #67c23a;
-    }
-
-    .formStyle .mini .fa-check-circle-o .form-item-input{
-        border: 1px solid #67c23a;
-    }
-
-    .formStyle .medium .fa-times-circle-o .form-item-input{
-        border: 1px solid #f56c6c;
-    }
-
-    .formStyle .small .fa-times-circle-o .form-item-input{
-        border: 1px solid #f56c6c;
-    }
-
-    .formStyle .mini .fa-times-circle-o .form-item-input{
-        border: 1px solid #f56c6c;
-    }
-
-    .formStyle  .fa.fa-times-circle-o{
-        position: relative;
-    }
-
-    .formStyle  .fa.fa-check-circle-o{
-        position: relative;
-    }
-
-    /* 错误图标 */
-    .formStyle  .fa-times-circle-o:before{
-        content: "\f05c";
-        position: absolute;
-        top: 10px;
-        right: 8px;
-        font-size: 12px;
-        color: #f56c6c;
-    }
-
-    .formStyle .medium .fa-times-circle-o:before{
-        top: 15px;
-    }
-
-    .formStyle .small .fa-times-circle-o:before{
-        top: 12px;
-    }
-
-    .formStyle .mini .fa-times-circle-o:before{
-        top: 10px;
-    }
-
-    /* 正确图标 */
-    .formStyle  .fa-check-circle-o:before{
-        content: "\f05d";
-        position: absolute;
-        top: 10px;
-        right: 8px;
-        font-size: 12px;
-        color: #67c23a;
-    }
-
-    .formStyle .medium .fa-check-circle-o:before{
-        top: 15px;
-    }
-
-    .formStyle .small .fa-check-circle-o:before{
-        top: 12px;
-    }
-
-    .formStyle .mini .fa-check-circle-o:before{
-        top: 10px;
     }
 </style>

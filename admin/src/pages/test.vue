596a3aeb542bd2513cbbc90809562ff50b4b89c3
Stringbao <string_bao@163.com>
2019-05-23 17:14:41 +0800

    处理vue里面带html标签的问题

--- admin/src/pages/test.vue
+++ admin/src/pages/test.vue
@@ -3,6 +3,7 @@
 <template>
     <div>
         <baby-input ref='b1'></baby-input>
+        <span v-for="(item,index) in data" class='html' :key="index">{{item.name}}</span>
     </div>
 </template>
 
@@ -15,14 +16,40 @@ export default {
     components:{BabyInput},
     data(){
         return {
+            data:[],
         }
     },
     methods:{
-
+        replaceHTMLTag(str){
+            return str.replace(/\${2}(【[\u4e00-\u9fa5]+?】)\${2}/g, '<span style="color:red">$1</span>');
+        },
+        replaceCount(str){
+            return str.replace(/#(\w+?)#/g, '$1');
+        }
     },
     mounted(){
         window.setTimeout(()=>{
             this.$refs['b1'].setName("我是倩倩");
+
+            let data = [
+                {name:'按客户付款方$$【数学启蒙课】$$按时打卡空间但是阿萨德呢'},
+                {name:'按客户付款方#rhymeCount#按时打卡空间但是阿萨德呢'}
+            ]
+            data.forEach(item=>{
+                let str = this.replaceHTMLTag(item.name);
+                str = this.replaceCount(str);
+                item.name = str;
+            })
+            this.data = data;
+        },0)
+
+        window.setTimeout(()=>{
+            let spans = document.getElementsByTagName("span");
+            for(let i = 0;i<spans.length;i++){
+                let str = spans[i].textContent;
+                spans[i].innerHTML = str;
+                console.log(str);
+            }
         },1000)
     }
 }

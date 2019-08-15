ffd0188d5d9e3e1524ab8fae7408bb8975fb5312
Stringbao <string_bao@163.com>
2019-08-15 22:00:41 +0800

    3

--- admin/src/core/comps/table/body.vue
+++ admin/src/core/comps/table/body.vue
@@ -12,7 +12,6 @@
                 </td>
                 <td class="opration" v-if="actions && actions.length != 0">
                     <div v-for="(x,i) in actions" class="btnContent" :key="i">
-                        <!-- <button v-if="actionShowFn(x,row)" :class="x.key" class="btn btn-sm btn-link" @click.prevent="e=>{x.action(row)}" >{{x.val}}</button> -->
                         <le-button v-if="actionShowFn(x,row)" :type="x.key" @click="e=>{x.action(row)}" :value="x.val"></le-button>
                     </div>
                 </td>

48d4fe95c7463d8a4f0e03508cdb7e3b9757264b
Stringbao <string_bao@163.com>
2019-08-23 15:27:36 +0800

    3

--- admin/src/core/comps/select/localSelect.vue
+++ admin/src/core/comps/select/localSelect.vue
@@ -312,18 +312,6 @@
   }
 </script>
 <style scoped>
-    input.searchMsg::-webkit-input-placeholder { /* WebKit browsers */
-        color: #b7b8bb;
-    }
-    input.searchMsg:-moz-placeholder { /* Mozilla Firefox 4 to 18 */
-        color: #b7b8bb;
-    }
-    input.searchMsg::-moz-placeholder { /* Mozilla Firefox 19+ */
-        color: #b7b8bb;
-    }
-    input.searchMsg:-ms-input-placeholder { /* Internet Explorer 10+ */
-        color: #b7b8bb;
-    }
     .blueborder{
         border-color:#409eff !important;
     }

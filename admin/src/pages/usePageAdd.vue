98c9603ef4e663255bf6a6cba7c7bb21a1e17000
string_bao <string_bao@163.com>
2019-09-01 17:32:17 +0800

    fix bug

--- admin/src/pages/usePageAdd.vue
+++ admin/src/pages/usePageAdd.vue
@@ -19,6 +19,16 @@
 
             <le-local-select label="模糊搜索" :dataSource="dataSource" multiple v-model="form1.s1" display-name="name" msg="下拉框必填" display-value="code" on></le-local-select> 
 
+            <le-date-time-picker label="开始时间" msg="开始时间必填" v-model="form1.dt1" on></le-date-time-picker>
+            
+            <le-date-time-picker label="结束时间" msg="结束时间必填" v-model="form1.dt2" on></le-date-time-picker>
+
+            <le-date-picker label="日期组件"  v-model="form1.d1" msg="日期不允许为空" on></le-date-picker>
+            
+            <le-time-picker label="时间组件" v-model="form1.t1" msg="时间不允许为空" on></le-time-picker>
+
+            <le-local-select label="模糊搜索" :dataSource="dataSource" multiple v-model="form1.s1" display-name="name" msg="下拉框必填" display-value="code" on></le-local-select> 
+
             <le-radio-list label="性别" :data-source="sexDataSource" v-model="form1.sex" display-name="name" msg="单选框必填" display-value="code" on></le-radio-list>
 
             <le-checkbox-list label="爱好" :data-source="favDataSource" @change='changecks' ref='hobby' v-model="form1.hobby" display-name="name" msg="复选框必填1" display-value="code" on></le-checkbox-list>
